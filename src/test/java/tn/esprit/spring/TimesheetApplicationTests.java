aa2823cba010f80f61a70d0b172f76f4f1c42003
houssemrdissi <houssem.rdissi@esprit.tn>
2020-10-25 14:26:14 +0100

    houss58

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -54,8 +54,8 @@ public class TimesheetApplicationTests {
 	@Test
 	public void testdeleteEntreprise() throws ParseException {
 
-		us.deleteEntrepriseById(60); 		
-	    assertNull(us.getEntrepriseById(60));			
+		us.deleteEntrepriseById(61); 		
+	    assertNull(us.getEntrepriseById(61));			
 	}
 	
 	@Test
@@ -68,7 +68,7 @@ public class TimesheetApplicationTests {
 	public void testRetrieveAllEntreprise() {
 		
 		List<Entreprise> Entreprises = us.retrieveAllEntreprises(); 
-		assertEquals(61, Entreprises.size());
+		assertEquals(70, Entreprises.size());
 	}
 	
 	///////////////////////////////////////// FINISH HOUSSEM MODULE CRUD TEST ENTREPRISE /////////////////////////////
